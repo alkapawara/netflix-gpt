c5deb504465033bde787fc0d77ee0f53a69916e3
alkapawara <alkapawara749@gmail.com>
2025-03-27 15:26:13 +0530

    test

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTION } from "../utils/constants";
 const Browse = () => {
-  const getNowMoviePlay = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTION
-    );
-    const json = await data.json();
-    console.log(json);
-  };
+  // const getNowMoviePlay = async () => {
+  //   const data = await fetch(
+  //     "https://api.themoviedb.org/3/movie/now_playing?page=1",
+  //     API_OPTION
+  //   );
+  //   const json = await data.json();
+  //   console.log("json", json);
+  // };
+  // useEffect(() => {
+  //   getNowMoviePlay();
+  // }, []);
   return (
     <>
       <Header />
