ad158f58003dc5e10ca3541454d752051bf28da9
alkapawara <alkapawara749@gmail.com>
2025-03-27 15:28:51 +0530

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,50 +6,58 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { useEffect } from "react";
-import {onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 const Header = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const user = useSelector((state) => state.user);
 
-  const handlSignOut=()=>{
-    signOut(auth).then(() => {
-      // Sign-out successful.
-     
-    }).catch((error) => {
-      // An error happened.
-      console.error("Sign out error:", error);
-    });
-  }
- useEffect(()=>{
-  const unsubscribe= onAuthStateChanged(auth, (user) => {
+  const handlSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+        console.error("Sign out error:", error);
+      });
+  };
+  useEffect(() => {
+    console.log("🔍 Checking auth state...", user);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
-        const {uid,email,displayName,photoURL} = user;
+        const { uid, email, displayName, photoURL } = user;
 
-        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
         // ...
-        navigate('/browse')
+        navigate("/browse");
       } else {
         // User is signed out
-        dispatch(removeUser())
-        navigate('/')
+        dispatch(removeUser());
+        navigate("/");
       }
     });
     // unsubscribe when component unmount
-    return()=> unsubscribe()
-  },[])
-  ;
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="absolute z-10 px-32 w-full bg-gradient-to-t from-transparent to-black flex justify-between items-center">
       <img src={logo} alt="Netflix Logo" className="w-44 " />
-   {user && (
-      <div className="flex gap-3 items-center">
-      <img src={user?.photoURL} className="w-10 h-10" alt="profile"/>
-      <button onClick={handlSignOut} >sign out</button>
-       </div> 
-   )}
+      {user && (
+        <div className="flex gap-3 items-center">
+          <img src={user?.photoURL} className="w-10 h-10" alt="profile" />
+          <button onClick={handlSignOut}>sign out</button>
+        </div>
+      )}
     </div>
   );
 };
