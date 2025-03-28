4dd623390e7d077469ec5b7b634e5cf1f6c1bea9
alkapawara <alkapawara749@gmail.com>
2025-03-28 22:51:40 +0530

    video

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -9,11 +9,10 @@ const VideoBackground = ({ moviId }) => {
     <div className="w-screen">
       {trailerVideo ? (
         <iframe
-         className="w-screen aspect-video"
-          src={`https://www.youtube.com/embed/${trailerVideo?.key}?mute=1&autoplay=1&controls=0`}
+          className="w-screen aspect-video"
+          src={`https://www.youtube.com/embed/${trailerVideo?.key}?mute=1&autoplay=1`}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-         
         ></iframe>
       ) : (
         <p>Loading trailer...</p>
