f54531a542e2155d9460c3ae25feec588180125d
alkapawara <alkapawara749@gmail.com>
2025-03-29 20:27:19 +0530

    test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { MOVIE_IMG_CDN_URL } from '../utils/constants'
-const MovieCard = ({posterPath}) => {
+import React from "react";
+import { MOVIE_IMG_CDN_URL } from "../utils/constants";
+const MovieCard = ({ posterPath }) => {
   return (
-    <div className='w-48 pr-6'>
-        <img src={MOVIE_IMG_CDN_URL + posterPath} alt="" />
+    <div className="w-48 pr-6">
+      <img src={MOVIE_IMG_CDN_URL + posterPath} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
